1f12d78f216b9b44de8e1d7569d8ca52b2ff8928
ssing263 <98934637+ssing263@users.noreply.github.com>
2022-06-10 00:06:27 -0700

    Adjusting percentage sign position

--- NearestNeighbour.py
+++ NearestNeighbour.py
@@ -13,8 +13,7 @@ def getAccuracy(selectFeaturesIndices):
     for i in range(rows):
         testLabel, testfeatures = label[i], selectFeatures[i, :]
         trainLabel, trainfeatures = np.delete(label, i, 0), np.delete(selectFeatures, i, 0)
-        predLabel = trainLabel[np.argmin(np.sqrt(np.sum(np.square(trainfeatures - testfeatures), axis=1)))].item()
-        if testLabel == predLabel:
+        if testLabel == trainLabel[np.argmin(np.sqrt(np.sum(np.square(trainfeatures - testfeatures), axis=1)))].item():
             correctCount += 1
     return 100 * correctCount / rows
 
@@ -30,25 +29,25 @@ def forwardSelection(numOfFeatures):
             runningSet = currentFeatureSet + [f]
             runningSet.sort()
             runningSetAccuracy = getAccuracy(runningSet)
-            print("Using features(s) %s accuracy is %.2f%%" % (str(runningSet).replace("[", "{").replace("]","}"), runningSetAccuracy), end="\n")
+            print("Using features(s) %s accuracy is %.2f %%" % (str(runningSet).replace("[", "{").replace("]","}"), runningSetAccuracy), end="\n")
             if runningSetAccuracy > expandingBestFeatureGroup[1]:
                 expandingBestFeatureGroup = [runningSet, runningSetAccuracy, f]
-        print("\nFeature set %s was best, accuracy is %.2f%%" % (str(expandingBestFeatureGroup[0]).replace("[", "{").replace("]","}"), expandingBestFeatureGroup[1]), end="\n\n")
+        print("\nFeature set %s was best, accuracy is %.2f %%" % (str(expandingBestFeatureGroup[0]).replace("[", "{").replace("]","}"), expandingBestFeatureGroup[1]), end="\n\n")
         currentFeatureSet = expandingBestFeatureGroup[0]
         remainingFeatures.remove(expandingBestFeatureGroup[2])
         if expandingBestFeatureGroup[1] > bestFeatureGroup[1]:
             bestFeatureGroup = [expandingBestFeatureGroup[0], expandingBestFeatureGroup[1]]
         else:
             print("(Warning, Accuracy has decreased! Continuing search in case of local maxima)")
-    print("Finished search!! The best feature subset is %s, which has an accuracy of %.2f%%" %(str(bestFeatureGroup[0]).replace("[", "{").replace("]","}"),bestFeatureGroup[1]))
+    print("Finished search!! The best feature subset is %s, which has an accuracy of %.2f %%" %(str(bestFeatureGroup[0]).replace("[", "{").replace("]","}"),bestFeatureGroup[1]))
 
 
 def backwardSelection(numOfFeatures):
     print("Beginning Search.", end="\n\n")
     currentFeatureSet = list(range(1, numOfFeatures + 1))
     bestFeatureGroup = [currentFeatureSet, getAccuracy(currentFeatureSet)]
-    print("Using features(s) %s accuracy is %.2f%%" % (str(bestFeatureGroup[0]).replace("[", "{").replace("]", "}"), bestFeatureGroup[1]), end="\n")
-    print("\nFeature set %s was best, accuracy is %.2f%%" % (str(bestFeatureGroup[0]).replace("[", "{").replace("]", "}"), bestFeatureGroup[1]), end="\n\n")
+    print("Using features(s) %s accuracy is %.2f %%" % (str(bestFeatureGroup[0]).replace("[", "{").replace("]", "}"), bestFeatureGroup[1]), end="\n")
+    print("\nFeature set %s was best, accuracy is %.2f %%" % (str(bestFeatureGroup[0]).replace("[", "{").replace("]", "}"), bestFeatureGroup[1]), end="\n\n")
     for i in range(numOfFeatures):
         reducingBestFeatureGroup = [None, float("-inf"), -1]
         for f in currentFeatureSet:
@@ -56,16 +55,16 @@ def backwardSelection(numOfFeatures):
             runningSet.remove(f)
             runningSet.sort()
             runningSetAccuracy = getAccuracy(runningSet)
-            print("Using features(s) %s accuracy is %.2f%%" % (str(runningSet).replace("[", "{").replace("]", "}"), runningSetAccuracy), end="\n")
+            print("Using features(s) %s accuracy is %.2f %%" % (str(runningSet).replace("[", "{").replace("]", "}"), runningSetAccuracy), end="\n")
             if runningSetAccuracy > reducingBestFeatureGroup[1]:
                 reducingBestFeatureGroup = [runningSet, runningSetAccuracy, f]
-        print("\nFeature set %s was best, accuracy is %.2f%%" % (str(reducingBestFeatureGroup[0]).replace("[", "{").replace("]", "}"), reducingBestFeatureGroup[1]), end="\n\n")
+        print("\nFeature set %s was best, accuracy is %.2f %%" % (str(reducingBestFeatureGroup[0]).replace("[", "{").replace("]", "}"), reducingBestFeatureGroup[1]), end="\n\n")
         currentFeatureSet.remove(reducingBestFeatureGroup[2])
         if reducingBestFeatureGroup[1] > bestFeatureGroup[1]:
             bestFeatureGroup = [reducingBestFeatureGroup[0], reducingBestFeatureGroup[1]]
         else:
             print("(Warning, Accuracy has decreased! Continuing search in case of local maxima)")
-    print("Finished search!! The best feature subset is %s, which has an accuracy of %.2f%%" % (
+    print("Finished search!! The best feature subset is %s, which has an accuracy of %.2f %%" % (
         str(bestFeatureGroup[0]).replace("[", "{").replace("]", "}"), bestFeatureGroup[1]))
 
 
